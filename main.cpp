b14bac3bba6061a83717c0ccd3f292b0b1324d19
pedro pizoni <pedro@kali.kalli>
2025-09-22 21:46:11 -0300

    criando desenho da estrela

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <vector>
 #include "text_pixel.hpp"
@@ -5,34 +6,27 @@
 int main() {
     std::vector<text_pixel> pixels;
 
-    int centerX = 15;
-    int centerY = 8;
-    int radius = 5; 
-
-    for (int y = centerY - radius; y <= centerY + radius; y++) {
-        for (int x = centerX - radius; x <= centerX + radius; x++) {
-            int dx = x - centerX;
-            int dy = y - centerY;
-            if (dx*dx + dy*dy <= radius*radius) {
-                if (y < centerY) {
-                    pixels.push_back(text_pixel(x, y, "red"));   // topo da pokebola 
-                } else if (y > centerY) {
-                    pixels.push_back(text_pixel(x, y, "white")); // parte de bauxo da pokebla
-                } else {
-                    pixels.push_back(text_pixel(x, y, "black")); // parte do meio da pokebola
-                }
-            }
-        }
-    }
-
-   
-    pixels.push_back(text_pixel(centerX, centerY, "black")); // botao da pokebola
-    pixels.push_back(text_pixel(centerX, centerY, "white")); // botao da pokebola
+    for (int x = 14; x <= 16; x++) pixels.push_back(text_pixel(x, 5, "yellow"));
+    for (int x = 12; x <= 18; x++) pixels.push_back(text_pixel(x, 6, "yellow"));
+    for (int x = 11; x <= 19; x++) pixels.push_back(text_pixel(x, 7, "yellow"));
+    for (int x = 10; x <= 20; x++) pixels.push_back(text_pixel(x, 8, "yellow"));
+    for (int x = 10; x <= 20; x++) pixels.push_back(text_pixel(x, 9, "yellow"));
+    for (int x = 11; x <= 19; x++) pixels.push_back(text_pixel(x, 10, "yellow"));
+    for (int x = 12; x <= 18; x++) pixels.push_back(text_pixel(x, 11, "yellow"));
+    for (int x = 14; x <= 16; x++) pixels.push_back(text_pixel(x, 12, "yellow"));
+    pixels.push_back(text_pixel(9, 8, "yellow"));
+    pixels.push_back(text_pixel(21, 8, "yellow"));
+    pixels.push_back(text_pixel(15, 4, "yellow"));
+    pixels.push_back(text_pixel(15, 13, "yellow"));
+    pixels.push_back(text_pixel(10, 5, "yellow"));
+    pixels.push_back(text_pixel(20, 5, "yellow"));
+    pixels.push_back(text_pixel(10, 11, "yellow"));
+    pixels.push_back(text_pixel(20, 11, "yellow"));
 
     for (auto &p : pixels) {
         p.draw();
     }
 
-    std::cout << "\033[20;0H"; 
+    std::cout << "\033[20;0H"; // move o cursor
     return 0;
 }
